d5e20ea8ea4c96840eab160483a42b4b665ec76e
Robert Leifke <r.leifke@gmail.com>
2024-07-10 22:50:18 -0400

    chore: config fixed

--- src/Exponential.sol
+++ src/Exponential.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.25;
 
-contract Foo {
+contract Exponential is ERC20 {
     function id(uint256 value) external pure returns (uint256) {
         return value;
     }
